f78a5447f1d88f5c7396f2a3cb12d8355a09c080
kevinmwilson <kwilson94@gmail.com>
2018-01-29 20:53:34 -0500

    finished LinkedList file for now

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,51 @@
-public class LinkedList {
-  
+/**
+ * A singly linked linked list that uses the Node class
+ */
+public class LinkedList<T> {
+
+  private Node<T> head;
+
+  public LinkedList() {}
+
+  public LinkedList(Node<T> head) {
+    this.head = head;
+  }
+
+  public Node<T> getHead() {
+    return this.head;
+  }
+
+  public void setHead(Node<T> other) {
+    this.head = other;
+  }
+
+  public void append(Node<T> n) {
+
+    // an iterator to scan the list
+    Node<T> iter = this.head;
+
+    while(iter.getNext() != null) {
+      iter = iter.getNext();
+    }
+
+    // iter is now the tail
+    iter.setNext(n);
+  }
+
+  @Override
+  public String toString() {
+    Node<T> iter = this.head;
+
+    // hardcoded output string capacity for example's sake
+    StringBuilder out = new StringBuilder(100);
+
+    while(iter.getNext() != null) {
+      out.append(iter).append("->");
+      iter = iter.getNext();
+    }
+
+    out.append(iter);
+
+    return out.toString();
+  }
 }
