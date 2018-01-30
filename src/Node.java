327a5338f47f73f2564f175e452da225d036cce6
kevinmwilson <kwilson94@gmail.com>
2018-01-29 20:42:20 -0500

    completed Node class

--- src/Node.java
+++ src/Node.java
@@ -1,7 +1,35 @@
 /**
  *
- * Simple node class for storing data.
+ * Simple one-directional node class for storing data.
  */
 public class Node<T> {
-  
+
+  /**
+   * The data that this node will hold
+   */
+  private T data;
+
+  /**
+   * The Node that this node points to.
+   */
+  private Node<T> next;
+
+  public Node() {}
+
+  public Node(T data, Node<T> next) {
+    this.data = data;
+    this.next = next;
+  }
+
+  public void setData(T data) {
+    this.data = data;
+  }
+
+  public T getData() {
+    return this.data;
+  }
+
+  public Node<T> getNext() {
+    return this.next;
+  }
 }
