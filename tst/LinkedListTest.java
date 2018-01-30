7a2fd2df633146d2c972ea8ddd5d48a31f73f88d
kevinmwilson <kwilson94@gmail.com>
2018-01-29 21:16:58 -0500

    added two test cases

--- tst/LinkedListTest.java
+++ tst/LinkedListTest.java
@@ -5,7 +5,22 @@ import static org.junit.Assert.*;
 public class LinkedListTest {
 
   @Test
+  public void addNodeToNullListGetDataTest() {
+    LinkedList<Integer> list = new LinkedList<>();
+    list.append(new Node<>(5));
+
+    assertEquals((Integer) 5, list.getHead().getData());
+  }
+
+  @Test
   public void toStringTest() {
-    
+    LinkedList<String> list = new LinkedList<>(new Node<>("Hello"));
+    list.append(new Node<>("how"));
+    list.append(new Node<>("are"));
+    list.append(new Node<>("you"));
+
+    String expected = "Hello->how->are->you";
+
+    assertEquals(expected, list.toString());
   }
 }
